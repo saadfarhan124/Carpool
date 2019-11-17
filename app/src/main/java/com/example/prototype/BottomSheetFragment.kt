7fd7817b368f8fd9a688249952923cf7e9794d59
5aad <saad_anwer@live.com>
2019-11-17 09:37:58 +0500

    All Done Bro

---
+++ app/src/main/java/com/example/prototype/BottomSheetFragment.kt
@@ -0,0 +1,15 @@
+package com.example.prototype
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class BottomSheetFragment: BottomSheetDialogFragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View =
+        inflater.inflate(R.layout.activity_returnbooking_bottomsheet, container, false)
+}
---
+++ app/src/main/java/com/example/prototype/TicketActvity.kt
@@ -0,0 +1,22 @@
+package com.example.prototype
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import kotlinx.android.synthetic.main.activity_ticket.*
+
+class TicketActvity: AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ticket)
+
+
+        btn_return.setOnClickListener {
+            val view = layoutInflater.inflate(R.layout.activity_returnbooking_bottomsheet, null)
+            val dialog = BottomSheetDialog(this)
+            dialog.setContentView(view)
+            dialog.show()
+        }
+    }
+}
