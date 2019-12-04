36b9cd959c955f196f7834b920038d48b211e99f
Saad Anwar <saad_anwer@live.com>
2019-12-05 04:16:24 +0500

    change pass back button

--- app/src/main/java/com/example/prototype/UpdatePassword.kt
+++ app/src/main/java/com/example/prototype/UpdatePassword.kt
@@ -2,6 +2,7 @@ package com.example.prototype
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 
 class UpdatePassword : AppCompatActivity() {
 
@@ -10,5 +11,18 @@ class UpdatePassword : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_uppass)
+
+        //Top App Bar
+        val toolbar: Toolbar = findViewById(R.id.toolbarupass)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+
+    }
+
+    //Top App Bar Back Nav
+    override fun onSupportNavigateUp():Boolean {
+        onBackPressed()
+        return true
     }
 }
