5a01cbe7cd466a40e67d8a50e3b6e0eb54a4a475
saadfarhan124 <saadfarhan124@gmail.com>
2019-12-05 04:14:02 +0500

    -

--- app/src/main/java/com/example/prototype/UpdatePassword.kt
+++ app/src/main/java/com/example/prototype/UpdatePassword.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 
 class UpdatePassword : AppCompatActivity() {
 
+//    private lateinit var
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_uppass)
