cbd6f53173182df9a4f7039629af2eafb7c7c1f9
Saad <saad_anwer@live.com>
2019-12-13 21:06:46 +0500

    feedback activity tab functionality done

--- app/src/main/java/com/example/prototype/ui/feedback/FeedbackFragment.kt
+++ app/src/main/java/com/example/prototype/ui/feedback/FeedbackFragment.kt
@@ -6,7 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
+import androidx.viewpager.widget.ViewPager
 import com.example.prototype.R
+import com.example.prototype.adapters.FeedbackSectionPageAdapter
+import com.google.android.material.tabs.TabLayout
+import kotlinx.android.synthetic.main.fragment_feedback.*
 
 class FeedbackFragment:Fragment() {
 
@@ -24,6 +28,19 @@ class FeedbackFragment:Fragment() {
 //        galleryViewModel.text.observe(this, Observer {
 //            textView.text = it
 //        })
+
+//        val feedbackSectionPageAdapter = FeedbackSectionPageAdapter(root.context, supportFragmentManager)
+//        val viewPager: ViewPager =root.findViewById(R.id.view_pager)
+//        viewPager.adapter = feedbackSectionPageAdapter
+//        val tabs: TabLayout = root.findViewById(R.id.feedback_tabs)
+//        tabs.setupWithViewPager(viewPager)
+
+        val feedbackSectionPageAdapter = FeedbackSectionPageAdapter(root.context,childFragmentManager)
+        val viewPager: ViewPager =root.findViewById(R.id.view_pager)
+        viewPager.adapter = feedbackSectionPageAdapter
+        val tabs: TabLayout = root.findViewById(R.id.feedback_tabs)
+        tabs.setupWithViewPager(viewPager)
+
         return root
     }
 }
