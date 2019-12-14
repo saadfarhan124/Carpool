a5b980476a0d4a089b40bea5f1c21f5ccf8e0f2f
Saad <saad_anwer@live.com>
2019-12-15 03:12:58 +0500

    new spinner experiment done

--- app/src/main/java/com/example/prototype/MainActivity.kt
+++ app/src/main/java/com/example/prototype/MainActivity.kt
@@ -4,11 +4,29 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.widget.Toolbar
 
+import android.content.Context
+import android.view.View
+import android.widget.AutoCompleteTextView
+import android.widget.ArrayAdapter
+
+import java.security.AccessController.getContext
+
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dob)
+        setContentView(R.layout.activity_suggestion)
+        val actv: AutoCompleteTextView = findViewById(R.id.outline_exposed_dropdown)
+        val COUNTRIES = arrayOf("Item 1", "Item 2", "Item 3", "Item 4")
+
+
+        val adapter = ArrayAdapter<String>(this, R.layout.activity_customspiner_item, COUNTRIES)
+
+        val editTextFilledExposedDropdown = actv
+        editTextFilledExposedDropdown.setAdapter(adapter)
+
+
 
 //        //Top App Bar
 //        val toolbar: Toolbar = findViewById(R.id.toolbarprofile)
--- app/src/main/java/com/example/prototype/SuggestionFragment.kt
+++ app/src/main/java/com/example/prototype/SuggestionFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import androidx.fragment.app.Fragment
 
 class SuggestionFragment : Fragment() {
@@ -14,6 +16,16 @@ class SuggestionFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater!!.inflate(R.layout.activity_suggestion, container, false)
+        val root = inflater!!.inflate(R.layout.activity_suggestion, container, false)
+
+        val actv: AutoCompleteTextView = root.findViewById(R.id.outline_exposed_dropdown)
+        val COUNTRIES = arrayOf("Item 1", "Item 2", "Item 3", "Item 4")
+
+
+        val adapter = ArrayAdapter<String>(root.context, R.layout.activity_customspiner_item, COUNTRIES)
+
+        val editTextFilledExposedDropdown = actv
+        editTextFilledExposedDropdown.setAdapter(adapter)
+        return root
     }
 }
