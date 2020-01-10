a7214546479643756a2e9b49ed3b909c0ec7b46c
Saad <saad_anwer@live.com>
2020-01-10 20:42:00 +0500

    Information Activity image color

--- app/src/main/java/com/example/prototype/ReviewInformationActivity.kt
+++ app/src/main/java/com/example/prototype/ReviewInformationActivity.kt
@@ -1,8 +1,11 @@
 package com.example.prototype
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings
+import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
 import com.example.prototype.Utilities.Util
@@ -10,10 +13,13 @@ import com.example.prototype.dataModels.CarSharingDataModel
 import com.example.prototype.dataModels.DaysDataModel
 import com.example.prototype.dataModels.ReviewInformationDataModel
 import com.google.android.material.chip.Chip
+import kotlinx.android.synthetic.main.activity_reviewinfo.*
 import org.jetbrains.anko.enabled
 import org.jetbrains.anko.onClick
 
 class ReviewInformationActivity : AppCompatActivity() {
+
+    private var MIC_STATUS = 0
 
     //Text Views
     private lateinit var txt_pickupAddress:TextView
@@ -37,8 +43,9 @@ class ReviewInformationActivity : AppCompatActivity() {
     private lateinit var ReviewData:ReviewInformationDataModel
     private lateinit var TimeDetails: List<CarSharingDataModel>
 
-
-
+    //Image View
+    private lateinit var ImageAc: ImageView
+    private lateinit var ImageNonAc: ImageView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -75,6 +82,33 @@ class ReviewInformationActivity : AppCompatActivity() {
 
         txt_startTime = findViewById(R.id.txt_startTime)
         txt_returnTime = findViewById(R.id.txt_returnTime)
+
+        //Image
+        ImageAc = findViewById(R.id.img_ac)
+        ImageNonAc = findViewById(R.id.img_nonAC)
+
+        ImageAc!!.setOnClickListener(object:View.OnClickListener {
+            override fun onClick(v: View?) {
+                if(MIC_STATUS == 0){
+                    img_ac.setColorFilter(Color.rgb(255,42,72))
+                    MIC_STATUS = 1
+                }else if(MIC_STATUS == 1){
+                    img_ac.setColorFilter(Color.WHITE)
+                    MIC_STATUS = 0
+                }
+            }})
+
+        ImageNonAc!!.setOnClickListener(object:View.OnClickListener {
+            override fun onClick(v: View?) {
+                if(MIC_STATUS == 0){
+                    ImageNonAc.setColorFilter(Color.rgb(255,42,72))
+                    MIC_STATUS = 1
+                }else if(MIC_STATUS == 1){
+                    ImageNonAc.setColorFilter(Color.WHITE)
+                    MIC_STATUS = 0
+                }
+            }})
+
 
         //Chips
         txt_chip_Mon = findViewById(R.id.txt_chip_Mon)
