b83023ab1db56082e06b127ab6245a9272c0f724
saadfarhan124 <saadfarhan124@gmail.com>
2020-01-19 00:11:08 +0500

    Ripple Effect Checked Moved google maps api key from debug to strings

---
+++ app/src/main/java/com/example/prototype/SelectGenderAndDob.kt
@@ -0,0 +1,17 @@
+package com.example.prototype
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.skyfishjy.library.RippleBackground
+import org.jetbrains.anko.onClick
+
+class SelectGenderAndDob : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gender)
+
+
+    }
+}
