e8240054a02b24977b1670382bab52727472cfea
Saad <saad_anwer@live.com>
2020-01-20 20:46:01 +0500

    custom dialog

---
+++ app/src/main/java/com/example/prototype/adapters/TimeDialogSectionPageAdapter.kt
@@ -0,0 +1,37 @@
+package com.example.prototype.adapters
+
+import android.content.Context
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.example.prototype.TimeDialogFragments.DropoffTimerFragment
+import com.example.prototype.TimeDialogFragments.PickupTimerFragment
+
+class TimeDialogSectionPageAdapter  (private val context: Context, fm: FragmentManager) :
+    FragmentPagerAdapter(fm) {
+
+    override fun getItem(position: Int): Fragment {
+        return when (position) {
+            0 -> {
+                PickupTimerFragment()
+            }
+            else -> {
+                return DropoffTimerFragment()
+            }
+        }
+    }
+
+    override fun getCount(): Int {
+        return 2
+    }
+
+    override fun getPageTitle(position: Int): CharSequence {
+        return when (position) {
+            0 -> "Pickup"
+            else ->{
+                return "Drop off"
+            }
+
+        }
+    }
+}
