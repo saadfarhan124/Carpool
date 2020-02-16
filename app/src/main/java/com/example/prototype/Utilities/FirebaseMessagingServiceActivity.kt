3faad11eb998f5b756a197d8767dc77caebbe6d3
Saad <saad_anwer@live.com>
2020-02-17 01:32:18 +0500

    get notification token

--- app/src/main/java/com/example/prototype/Utilities/FirebaseMessagingServiceActivity.kt
+++ app/src/main/java/com/example/prototype/Utilities/FirebaseMessagingServiceActivity.kt
@@ -7,7 +7,7 @@ import com.google.firebase.messaging.FirebaseMessagingService
 class FirebaseMessagingServiceActivity: FirebaseMessagingService() {
 
     override fun onNewToken(token: String?) {
-        Log.d("see", "Refreshed token: " + token!!)
+        Log.d("saaaad", "Refreshed token: " + token!!)
 
         // If you want to send messages to this application instance or
         // manage this apps subscriptions on the server side, send the
